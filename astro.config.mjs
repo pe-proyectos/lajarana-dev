8a3d8dd84a435d301a762e19ab3d756d97a9332a
Maira <maira@luminari.agency>
2026-03-26 16:33:52 +0000

    prod: migrate to lajarana.luminari.agency, remove early access

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,12 +5,12 @@ import node from '@astrojs/node';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://dev-lajarana.luminari.agency',
+  site: 'https://lajarana.luminari.agency',
   integrations: [react(), sitemap({
     filter: (page) => !page.includes('/mi-cuenta/') && !page.includes('/checkout/') && !page.includes('/login') && !page.includes('/register'),
     customPages: [
-      'https://dev-lajarana.luminari.agency/',
-      'https://dev-lajarana.luminari.agency/eventos',
+      'https://lajarana.luminari.agency/',
+      'https://lajarana.luminari.agency/eventos',
     ],
   })],
   adapter: node({ mode: 'standalone' }),
